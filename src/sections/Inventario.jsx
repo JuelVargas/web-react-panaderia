a877a1fae2cf7cc71dd8069d797c31c793ed6314
Juel-Vargas <juelvargasrojas@gmail.com>
2024-06-08 15:45:27 -0400

    arrega: estilos en Inventario.jsx y agrega imagenes de ingredientes

--- src/sections/Inventario.jsx
+++ src/sections/Inventario.jsx
@@ -6,37 +6,53 @@ const Inventario = () => {
       id="inventario"
       className="w-full flex justify-center"
     >
-      <div className="mx-3 border-2 border-slate-200 border-dashed p-2 rounded-lg bg-slate-100 mt-4 sm:mt-10 sm:w-[500px] lg:mt-20 lg:w-3/6 lg:p-10">
+      <div className="mx-3 border-2 border-slate-200 border-dashed p-2 rounded-lg bg-slate-100 mt-4 sm:mt-8 sm:w-[500px] lg:mt-20 lg:w-3/6 lg:p-10">
 
-        <div 
-        className="flex justify-between items-center my-2  px-4 py-4  bg-slate-400 rounded-xl"
+        <div
+          className="flex justify-between items-center mb-4  px-4 py-4  bg-slate-300 rounded-xl"
         >
           <h3
             className="text-xl h-full flex items-center w-4/6 rounded-e-full "
           >
             Nombre
           </h3>
-          <p
-            className="text-xl"
-          >
-            Cantidad
-          </p>
+          <div className="flex w-2/5 gap-2 justify-evenly">
+            <p
+              className="sm:text-xl"
+            >
+              Cantidad
+            </p>
+            <p className="sm:text-xl">
+              Unidad
+            </p>
+          </div>
         </div>
 
 
         {
           inventario.map((item) => (
             <div key={item.id} className="flex justify-between items-center my-2  px-4 py-2 bg-primary/15 odd:bg-secondary/30 ">
+              <img
+                src={item.imgURL}
+                width={40}
+                height={40}
+                className="shadow-lg"
+              />
               <h3
-                className="text-xl h-full flex items-center w-4/6 rounded-e-full "
+                className="text-xl h-full flex items-center w-4/6 rounded-e-full  ml-2"
               >
                 {item.nombre}:
               </h3>
-              <p
-                className="text-xl"
-              >
-                {item.cantidad}
-              </p>
+              <div className="flex justify-evenly w-2/5">
+                <p
+                  className="text-xl"
+                >
+                  {item.cantidad}
+                </p>
+                <p>
+                  {item.unidad}
+                </p>
+              </div>
             </div>
           ))
         }
